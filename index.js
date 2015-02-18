d8320f02e90ad02dc4dcb22b911628ccb4efb35f
groupbuilder <jianqun.chen.ch@gmail.com>
2015-02-18 08:13:04 +0000

    iframe test

--- index.js
+++ index.js
@@ -5,18 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-s = "";
-for (i = 2; i < 100; i++) {
-    for (j = 2; j * j <= i; j++) {
-        if (i % j == 0) {
-            break;
-        }
-    }
-    if (j * j > i) {
-        s += " " + i;
-    }
-}
-response.send(s);
+  response.send('<iframe src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1-tRENjHuydd9hCY8M3lZ0kOBv20LS9KlsUor7pe2DMU&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650" width="100%" height="650" frameborder="0"></iframe>')
   //response.send('Hello World!')
 })
 
