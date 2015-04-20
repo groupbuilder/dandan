a64ee1fa65bd20b6043057de195a2ff7bffee542
groupbuilder <jianqun.chen.ch@gmail.com>
2015-04-20 01:23:04 +0000

    Hello

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<iframe src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1-tRENjHuydd9hCY8M3lZ0kOBv20LS9KlsUor7pe2DMU&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650" width="100%" height="650" frameborder="0"></iframe>')
+  //response.send('<iframe src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1-tRENjHuydd9hCY8M3lZ0kOBv20LS9KlsUor7pe2DMU&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650" width="100%" height="650" frameborder="0"></iframe>')
   //response.send('Hello World!')
+  response.send('./capea/index.html')
 })
 
 app.listen(app.get('port'), function() {
