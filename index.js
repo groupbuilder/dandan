59c446d0d71d6634bec66471d1de28d489e7e0cb
groupbuilder <jianqun.chen.ch@gmail.com>
2015-04-21 04:56:55 +0000

    Write html

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   //response.send('<iframe src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=1-tRENjHuydd9hCY8M3lZ0kOBv20LS9KlsUor7pe2DMU&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650" width="100%" height="650" frameborder="0"></iframe>')
   //response.send('Hello World!')
-  response.send('./capea/index.html')
+  //response.send('./capea/index.html')
+  fs = require('fs')
+  fs.readFile('./caepa/index.html', function (err, html) {
+      if (err) {
+      throw err;
+      }       
+      response.write(html);
+  }
 })
 
 app.listen(app.get('port'), function() {
